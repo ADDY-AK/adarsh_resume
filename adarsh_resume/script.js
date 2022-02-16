f2372275fd57d6e073a516778067d8d6d1a085d5
ADDY-AK <adarsh.kumar_cs18@gla.ac.in>
2022-02-16 18:20:27 +0530

    done

---
+++ adarsh_resume/script.js
@@ -0,0 +1,44 @@
+// var lnStickyNavigation;
+
+// $(document).ready(function()
+// {
+// 	applyNavigation(); 
+// });
+
+// function applyNavigation()
+// {
+// 	applyNavigationFixForPhone();
+// 	applyStickyNavigation();
+// }
+
+// function applyNavigationFixForPhone()
+// {
+// 	$('.navbar li a').click(function(event) 
+// 	{
+// 		$('.navbar-collapse').removeClass('in').addClass('collapse');
+// 	});
+// }
+
+// function applyStickyNavigation()
+// {
+// 	lnStickyNavigation = $('.scroll-down').offset().top + 20;
+	
+// 	$(window).on('scroll', function() 
+// 	{  
+// 		stickyNavigation();  
+// 	});  
+	
+// 	stickyNavigation();
+// }
+
+// function stickyNavigation()
+// {         
+// 	if($(window).scrollTop() > lnStickyNavigation) 
+// 	{   
+// 		$('body').addClass('fixed');  
+// 	} 
+// 	else 
+// 	{  
+// 		$('body').removeClass('fixed');   
+// 	}  
+// }
